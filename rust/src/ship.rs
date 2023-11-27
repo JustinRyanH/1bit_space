678fe423d8b3921c9f7ff80422ed6ce11656f18f
Justin Hurstwright <jryanhurst@gmail.com>
2023-11-27 14:51:29 -0600

    ref: Extract method

--- rust/src/ship.rs
+++ rust/src/ship.rs
@@ -4,6 +4,7 @@ use godot::prelude::*;
 #[derive(GodotClass)]
 #[class(base=CharacterBody2D)]
 struct Ship {
+    #[export]
     speed: f64,
     #[export]
     angular_speed: f64,
@@ -16,13 +17,20 @@ struct Ship {
 impl ICharacterBody2D for Ship {
     fn init(base: Base<Self::Base>) -> Self {
         Self {
-            speed: 400.0,
+            speed: 2.0,
             angular_speed: 5.0,
             base,
         }
     }
 
     fn physics_process(&mut self, delta: f64) {
+        self.rotate_ship(delta);
+    }
+}
+
+#[godot_api]
+impl Ship {
+    fn rotate_ship(&mut self, delta: f64) {
         let input = Input::singleton();
         let mut base = self.base.clone();
         let rotate_axis = input.get_axis("Rotate Left".into(), "Rotate Right".into());
