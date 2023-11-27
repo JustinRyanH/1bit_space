3e31bd0c7a7517945fa711dc508ea2a7bcb6e167
Justin Hurstwright <jryanhurst@gmail.com>
2023-11-27 15:07:06 -0600

    ref: Extract method

--- rust/src/ship.rs
+++ rust/src/ship.rs
@@ -17,7 +17,7 @@ struct Ship {
 impl ICharacterBody2D for Ship {
     fn init(base: Base<Self::Base>) -> Self {
         Self {
-            linear_speed: 2.0,
+            linear_speed: 150.0,
             angular_speed: 5.0,
             base,
         }
@@ -25,16 +25,7 @@ impl ICharacterBody2D for Ship {
 
     fn physics_process(&mut self, delta: f64) {
         self.rotate_ship(delta);
-        let input = Input::singleton();
-        let movement_axis = input.get_action_strength("Accelerate".into());
-        let base_velocity = self.base.get_velocity();
-        if movement_axis > 0.0 {
-            let velocity_direction = Vector2::UP.rotated(self.base.get_rotation());
-            let new_velocity = base_velocity + (velocity_direction * movement_axis * (delta * self.linear_speed) as f32);
-            godot_print!("new_velocity: {:?}", new_velocity);
-            self.base.set_velocity(new_velocity);
-        }
-        self.base.move_and_slide();
+        self.move_ship(delta);
     }
 }
 
@@ -47,5 +38,17 @@ impl Ship {
 
         let base_rotation = base.get_rotation();
         base.set_rotation(base_rotation + (rotate_axis * (delta * self.angular_speed) as f32));
+    }
+
+    fn move_ship(&mut self, delta: f64) {
+        let input = Input::singleton();
+        let movement_axis = input.get_action_strength("Accelerate".into());
+        let base_velocity = self.base.get_velocity();
+        if movement_axis > 0.0 {
+            let velocity_direction = Vector2::UP.rotated(self.base.get_rotation());
+            let new_velocity = base_velocity + (velocity_direction * movement_axis * (delta * self.linear_speed) as f32);
+            self.base.set_velocity(new_velocity);
+        }
+        self.base.move_and_slide();
     }
 }
