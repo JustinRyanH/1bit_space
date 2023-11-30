74b01c35e24c785a3258c573c3912affbe19519b
Justin Hurstwright <jryanhurst@gmail.com>
2023-11-29 20:41:27 -0600

    ref: Move Struct to file

---
+++ rust/src/systems/bullet_spawn_system.rs
@@ -0,0 +1,44 @@
+use godot::bind::{godot_api, GodotClass};
+use godot::obj::{Base, Gd, UserClass};
+use godot::engine::{INode, load, Node, Node2D, PackedScene};
+use godot::builtin::{Callable, Vector2};
+use godot::log::godot_error;
+use godot::prelude::ToGodot;
+use crate::prelude::ProjectileMessageBus;
+
+#[derive(GodotClass)]
+#[class(base=Node2D)]
+pub struct BulletSpawnSystem {
+    #[export]
+    bullet_event_bus: Gd<ProjectileMessageBus>,
+    #[base]
+    base: Base<Node2D>,
+}
+
+#[godot_api]
+impl INode for BulletSpawnSystem {
+    fn init(base: Base<Self::Base>) -> Self {
+        Self { base, bullet_event_bus: ProjectileMessageBus::new_gd() }
+    }
+
+    fn ready(&mut self) {
+        let callable = Callable::from_object_method(&self.base.to_godot(), "spawn_bullet");
+        self.bullet_event_bus.connect("spawn_bullet".into(), callable);
+    }
+}
+
+#[godot_api]
+impl BulletSpawnSystem {
+    #[func]
+    pub fn spawn_bullet(&mut self, scene_path: String, position: Vector2, rotation: f32) {
+        let scene = load::<PackedScene>(&scene_path);
+        if let Some(scene) = scene.instantiate() {
+            let mut bullet = scene.cast::<Node2D>();
+            bullet.set_global_position(position);
+            bullet.set_rotation(rotation);
+            self.base.add_child(bullet.upcast::<Node>());
+        } else {
+            godot_error!("Could not build: {:?}", scene_path);
+        }
+    }
+}
--- rust/src/systems/mod.rs
+++ rust/src/systems/mod.rs
@@ -1,42 +1,5 @@
 mod projectile_system;
+mod bullet_spawn_system;
 
-use godot::prelude::*;
-
-use crate::prelude::*;
-
-#[derive(GodotClass)]
-#[class(base=Node2D)]
-pub struct BulletSpawnSystem {
-    #[export]
-    bullet_event_bus: Gd<ProjectileMessageBus>,
-    #[base]
-    base: Base<Node2D>,
-}
-
-#[godot_api]
-impl INode for BulletSpawnSystem {
-    fn init(base: Base<Self::Base>) -> Self {
-        Self { base, bullet_event_bus: ProjectileMessageBus::new_gd() }
-    }
-
-    fn ready(&mut self) {
-        let callable = Callable::from_object_method(&self.base.to_godot(), "spawn_bullet");
-        self.bullet_event_bus.connect("spawn_bullet".into(), callable);
-    }
-}
-
-#[godot_api]
-impl BulletSpawnSystem {
-    #[func]
-    pub fn spawn_bullet(&mut self, scene_path: String, position: Vector2, rotation: f32) {
-        let scene = load::<PackedScene>(&scene_path);
-        if let Some(scene) = scene.instantiate() {
-            let mut bullet = scene.cast::<Node2D>();
-            bullet.set_global_position(position);
-            bullet.set_rotation(rotation);
-            self.base.add_child(bullet.upcast::<Node>());
-        } else {
-            godot_error!("Could not build: {:?}", scene_path);
-        }
-    }
-}
+pub use projectile_system::*;
+pub use bullet_spawn_system::*;
