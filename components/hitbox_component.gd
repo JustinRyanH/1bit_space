23f6c02d2f64fdd1147ec36617867e6c9fb90089
Justin Hurstwright <jryanhurst@gmail.com>
2023-11-30 21:40:31 -0600

    fix: don't allow hitbox and hurtbox hit itself

--- components/hitbox_component.gd
+++ components/hitbox_component.gd
@@ -2,16 +2,16 @@ class_name HitboxComponent
 extends Area2D
 
 @export var damage: float = 1.0
-@export var actor: Node2D
 
-signal hit_hurtbox(hurtbox: HurtboxComponent, positon: Vector2)
+signal hit_hurtbox(hurtbox: HurtboxComponent)
 
 func _ready() -> void:
 	area_entered.connect(_on_hurtbox_entered)
 
 func _on_hurtbox_entered(hurtbox: HurtboxComponent) -> void:
 	if not hurtbox is HurtboxComponent: return
+	if hurtbox.get_parent() == get_parent(): return
 	if hurtbox.is_invincible: return
 	
-	hit_hurtbox.emit(hurtbox, actor.global_position)
+	hit_hurtbox.emit(hurtbox)
 	hurtbox.hurt.emit(self)
