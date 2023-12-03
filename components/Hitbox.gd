880df8cfa12d0e89b6dbdd2ca92df08c8fc79eb9
Justin Hurstwright <jryanhurst@gmail.com>
2023-12-03 13:44:28 -0600

    Add disabled property to Hitbox and Hurtbox classes

--- components/Hitbox.gd
+++ components/Hitbox.gd
@@ -2,6 +2,7 @@ class_name Hitbox
 extends Area2D
 
 @export var damage: int
+var disabled := false
 
 func _init() -> void:
 	collision_layer = 2
--- components/Hurtbox.gd
+++ components/Hurtbox.gd
@@ -1,6 +1,8 @@
 class_name Hurtbox
 extends Area2D
 
+var disabled := false
+
 func _init() -> void:
 	collision_layer = 0
 	collision_mask = 2
@@ -9,8 +11,10 @@ func _ready() -> void:
 	area_entered.connect(_on_area_entered)
 
 func _on_area_entered(hitbox: Hitbox) -> void:
+	if disabled: return
 	if hitbox == null: return
 	if hitbox.owner == owner: return
+	if hitbox.disabled: return
 	
 	if owner.has_method("take_damage"):
 		owner.take_damage(hitbox.damage)
