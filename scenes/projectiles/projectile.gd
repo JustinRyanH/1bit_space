56e5a5ce2eef161521018939f1a1314ae22304c0
Justin Hurstwright <jryanhurst@gmail.com>
2023-12-03 12:43:52 -0600

    ref: replace projectile mananager

--- scenes/projectiles/projectile.gd
+++ scenes/projectiles/projectile.gd
@@ -2,17 +2,18 @@ class_name Projectile
 extends Node2D
 
 @export var projectile_stats: ProjectileStates
-var speed_modifier = 0
+@export var velocity: Vector2 = Vector2.ZERO
 
 @onready var hitbox := $Hitbox as Hitbox
 
 func _ready() -> void:
 	hitbox.damage = projectile_stats.damage
+	
+func simple_setup(speed: float) -> void:
+	velocity = Vector2.UP.rotated(rotation) * projectile_stats.speed
 
 func _process(delta: float) -> void:
-	var vel = Vector2.UP.rotated(rotation) * (projectile_stats.speed + speed_modifier) * delta
-	print(vel)
-	translate(vel)
+	translate(velocity * delta)
 
 func take_damage(damage: int) -> void:
 	queue_free()
