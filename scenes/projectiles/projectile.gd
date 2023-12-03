d86afb0d243dd99f11a0bc95d5b7b9175cf4a2ff
Justin Hurstwright <jryanhurst@gmail.com>
2023-12-03 13:48:22 -0600

    don't let ship hit itself in the first few seconds

--- scenes/projectiles/projectile.gd
+++ scenes/projectiles/projectile.gd
@@ -5,12 +5,16 @@ extends Node2D
 @export var velocity: Vector2 = Vector2.ZERO
 
 @onready var hitbox := $Hitbox as Hitbox
+@onready var hurtbox: Hurtbox = $Hurtbox
 
 var ignore_targets := []
 var should_ignore_targets: bool = true
 
 func _ready() -> void:
 	hitbox.damage = projectile_stats.damage
+	hitbox.disabled = true
+	hurtbox.disabled = true
+	modulate.b = 0
 	
 func setup(initial_velocity: Vector2, direction: Vector2) -> void:
 	velocity = initial_velocity + (direction * projectile_stats.speed)
@@ -18,12 +22,13 @@ func setup(initial_velocity: Vector2, direction: Vector2) -> void:
 func _process(delta: float) -> void:
 	translate(velocity * delta)
 
-func take_damage(damage: int) -> void:
+func take_damage(_damage: int) -> void:
 	queue_free()
 
 func _on_projectile_life_timer_timeout() -> void:
 	queue_free()
 
-
-func stop_ignore_toggles() -> void:
-	should_ignore_targets = false
+func _on_ignore_timer_timeout() -> void:
+	hitbox.disabled = false
+	hurtbox.disabled = false
+	modulate.b = 1
